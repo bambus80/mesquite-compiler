0dce241c06a456f113f223f6a2812ca1574aa042
bambus <132773043+bambus80@users.noreply.github.com>
2024-07-07 15:03:27 +0200

    modify main.py and main.msq

--- main.py
+++ main.py
@@ -1,29 +1,34 @@
 from src.parsing import parse
 from src.scratch3.packager import generate_project
+from json import JSONEncoder
 import os
 import argparse
 
 from src.utils import pretty_print_program
 
-VERSION = "0.0.1a3"
+VERSION = "0.0.2"
 
 # TODO: Include tests !!!
 if __name__ == "__main__":
     print(f"\x1b[40;36m ━━━━━━ Mesquite Compiler version {VERSION} ━━━━━━")
     parser = argparse.ArgumentParser(prog='mesquite')
+    # TODO: Add support for compiling standalone Scratch sprites
     parser.add_argument('-i', '--input', type=str, required=False, default="./main.msq")
     parser.add_argument('-o', '--output', type=str, required=False, default="./main.sb3")
     args = parser.parse_args()
 
-    input_dir = os.path.join(os.getcwd(), args.input)
+    input_dir = os.path.normpath(os.path.join(os.getcwd(), args.input))
 
     project_cwd, _ = os.path.split(os.path.abspath(input_dir))
 
     with open(input_dir, 'r') as f:
         print(f"\x1b[40;36m {input_dir} \x1b[0;36m🭞🭜🭘\x1b[0m\nTokenizing...")
         tree = parse(f.read())
-        pretty_print_program(tree)
-        print("Transpiling...")
-        project = generate_project(tree, project_cwd)
-        pretty_print_program(project)
+        # pretty_print_program(tree)
+
+    print("Transpiling...")
+    project = generate_project(tree, project_cwd)
+
+    with open("./build/project.json", "w") as project_json:
+        project_json.write(JSONEncoder().encode(project.serialize()))
         # TODO: Zip and save to .SPRITE3 and print info about the compiled sprite (eg. sprite size)
